e4ebfc86c110a5e586e100374a36a96bbb3f9828
Michael Kafka <michael.kafka@thomsonreuters.com>
2014-04-25 14:00:50 -0400

    CODECANYON RELEASE :: packaged as 2.7.3

--- app/js/app.js
+++ app/js/app.js
@@ -45,6 +45,6 @@
     registerNamespace('PhotoMosaic.Plugins');
     registerNamespace('PhotoMosaic.ErrorChecks');
     registerNamespace('PhotoMosaic.Mosaics', []);
-    registerNamespace('PhotoMosaic.version', '2.7.2');
+    registerNamespace('PhotoMosaic.version', '2.7.3');
 
 }(jQuery, window));

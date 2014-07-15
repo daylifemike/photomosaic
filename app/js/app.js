5ddda983bb504d139e0c984cb96583ad1e565679
Michael Kafka <mike@daylife.com>
2014-07-15 09:48:49 -0400

    CODECANYON RELEASE :: packaged as 2.8.1

--- app/js/app.js
+++ app/js/app.js
@@ -44,6 +44,6 @@
     registerNamespace('PhotoMosaic.Plugins');
     registerNamespace('PhotoMosaic.ErrorChecks');
     registerNamespace('PhotoMosaic.Mosaics', []);
-    registerNamespace('PhotoMosaic.version', '2.8');
+    registerNamespace('PhotoMosaic.version', '2.8.1');
 
 }(jQuery, window));

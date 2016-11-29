04d3b823ee5c6dc3afcca7dc11a0b8537054b5ec
Michael Kafka <daylife.mike@gmail.com>
2016-11-29 14:34:54 -0500

    CODECANYON RELEASE : packaged as 2.15.7

--- app/includes/class-photomosaic.php
+++ app/includes/class-photomosaic.php
@@ -11,7 +11,7 @@ class Photomosaic {
 
     public function __construct () {
         $this->plugin_name = 'photomosaic';
-        $this->version = '2.15.6';
+        $this->version = '2.15.7';
 
         $this->load_dependencies();
         // $this->set_locale();
